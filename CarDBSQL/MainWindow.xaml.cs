e894f50d6ff8e6906eb3ed219821739e94110b2c
Павел <dirtkille23@esstu.onmicrosoft.com>
2022-06-02 20:22:44 +0800

    test

--- CarDBSQL/MainWindow.xaml.cs
+++ CarDBSQL/MainWindow.xaml.cs
@@ -20,9 +20,50 @@ namespace CarDBSQL
     /// </summary>
     public partial class MainWindow : Window
     {
+        CarDriverDBEntities context;
+        static int counter = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            context = new CarDriverDBEntities();
+           
         }
-    }
-}
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+            counter++;
+            int numDriverDocument = Convert.ToInt32(login.Text);
+            string pass = password.Text;
+            
+            Driver driver = context.Driver.ToList().Find(x => x.numDriverDocument == numDriverDocument);
+            if (counter >= 3)
+            {
+                login.IsEnabled = false;
+                login.Text = "";
+                password.IsEnabled = false;
+                password.Text = "";
+                LoginButton.IsEnabled = false;
+                MessageBox.Show("Превышено количество попыток(3)", "Критическая Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            }
+            if (driver == null)
+            {
+                MessageBox.Show("Данного водителя не существует!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            
+            }
+            else
+            {
+                if (driver.password.Equals(pass))
+                {
+                    MessageBox.Show("Вход выполнен!", "Успешно!", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Пароли не совпадают", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+                } 
+            }
+        }
